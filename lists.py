592c8f34e4221d1298901c59824a76fb63cdbd48
halitbarut <mhbarut66@gmail.com>
2023-08-03 13:58:56 +0300

    Lists Lesson 1

---
+++ lists.py
@@ -0,0 +1,18 @@
+# BTK Akademi Python Dersi 3.12_Pythonda Listeler
+
+message = 'Hello there. My name is Halit Barut.'.split()
+# print(message)
+
+myList = [1,2,3]
+
+list1 = ['one','two','three']
+list2 = ['four','five','six']
+
+numbers = list1 + list2
+
+print(len(numbers))
+print(len(numbers[0]))
+
+userA = ['Halit', 18]
+userB = ['Barut', 17]
+
