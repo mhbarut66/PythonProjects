cd82ac689adaece30c721788662d6101ca156f75
Halit <mhbarut66@gmail.com>
2023-08-13 15:04:53 +0300

    4.1

---
+++ assigment.py
@@ -0,0 +1,16 @@
+# BTK Akademi Python Dersleri 4.1_Atama Operatörleri
+
+x = 5
+y = 10
+z = 20
+
+x, y, z = 5, 10, 20
+
+x += 5 # x = x + 5
+
+values = 1,2,4
+
+x, y, z = values
+# values içindeki değerler sırasıyla x, y ve z'ye atandı.
+
+
