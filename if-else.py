1fffb627f45e1d00ec5becb740d079df6eb93a25
Halit <mhbarut66@gmail.com>
2023-08-14 23:28:13 +0300

    5.2

--- if-else.py
+++ if-else.py
@@ -11,3 +11,28 @@ if (username == 'mhbarut66'):
         print('parola yanlis')
 else:
     print('username yanlis')
+    
+# BTK Akademi Python Dersi 5.2_Koşullu Durum Blokları (elif)
+
+x = int(input('x: '))
+y = int(input("y: "))
+
+if x > y:
+    print('x y den büyük ')
+    
+elif x == y:
+    print('x y ye eşit')
+else:
+    print("x yden küçük")
+    
+    
+    
+num = int(input('sayı : '))
+
+if num > 0:
+    print('Sayı pozitif')
+elif num < 0:
+    print('Sayı negatif')
+else:
+    print('Sayı sıfır')
+    
