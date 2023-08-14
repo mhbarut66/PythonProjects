380c01faa3d2157c636ef6096c220c0468a9229e
Halit <mhbarut66@gmail.com>
2023-08-14 20:58:28 +0300

    4.7

---
+++ others.py
@@ -0,0 +1,19 @@
+# BTK Akademi Python Dersi 4.7_Diğer Python Operatörleri
+
+# Identity Operator : is
+
+x = y = [1, 2, 3]
+z = [1, 2, 3]
+
+print(x == y) # Burada elemanların aynı olup olmadığına bakılır.
+print(x == z)
+
+print(x is y) # Burada adresin aynı olup olmadığına bakılır.
+print(x is z)
+
+# Membership Operator : in
+
+x = ['apple', 'banana']
+print('banana' in x) # listenin içerisinde o elemanın olup olmadığına göre bir değer döndürür.
+
+
