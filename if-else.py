86ec409976ba897ee1a60dc711bbef06b5abb715
Halit <mhbarut66@gmail.com>
2023-08-14 21:04:23 +0300

    5.1

---
+++ if-else.py
@@ -0,0 +1,13 @@
+# BTK Akademi Python Dersi 5.1_Koşullu Durum Blokları
+
+username = 'mhbarut66'
+password = '1234'
+
+if (username == 'mhbarut66'):
+
+    if (password == '12345'):
+        print('Hos geldiniz')
+    else:
+        print('parola yanlis')
+else:
+    print('username yanlis')
