6accc64293606af3428a07079d974a5c1d114180
Halit <mhbarut66@gmail.com>
2023-08-16 16:48:38 +0300

    6.2 Questions

---
+++ for-demo.py
@@ -0,0 +1,19 @@
+# BTK Akademi Python Dersi 6.2_Pythonda for Döngüleri Uygulama
+
+sayilar = [1,3,5,7,9,12,19,21]
+# 1- Sayilar listesindeki hangi sayılar 3'ün katıdır
+# 2- Sayilar listesinde sayıların toplamı kaçtır?
+# 3- Sayilar listesindeki tek sayıların karesini alınız
+sehirler = ['kocaeli', 'istanbul', 'ankara','izmir', 'rize']
+# 4- Şehirlerden hangileri en fazla 5 karakterlidir
+urunler = [
+    {'name': 'samsung S6', 'price': '3000' },
+    {'name': 'samsung S7', 'price': '4000' },
+    {'name': 'samsung S8', 'price': '5000'},
+    {'name': 'Samsung S9', 'price': '6000'},
+    {'name': 'Samsung S10', 'price': '7000'}
+]
+
+# 5- Ürünlerin fiyatları toplamı nedir
+# 6- Ürünlerden fiyatı en fazla 5000 olan ürünler hangileridir
+
