535e71047daa6b0b965b2703835b44222702bd18
Mehmet Halit Barut <mhbarut66@gmail.com>
2023-09-03 16:04:13 +0300

    6.8 Questions

---
+++ loops-demo.py
@@ -0,0 +1,8 @@
+# BTK Akademi Python Dersleri 6.8_Uygulama - Sayı Tahmin Uygulaması
+
+'''
+    1-100 arasında rastgele üretilecek bir sayıyı aşağı yukarı ifadeleri ile buldurmaya çalışın.
+    ** 'random modülü' için 'python random' şeklinde arama yapın.
+    ** 100 üzerinden puanlama yapın. Her soru 20 puan.
+    ** Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı üzerinden hesaplansın.
+'''
