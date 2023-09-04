118461a648d9d98fff0c043373fed7ca7992321c
Mehmet Halit Barut <mhbarut66@gmail.com>
2023-09-04 22:46:04 +0300

    6.9 solved

--- asal-sayi.py
+++ asal-sayi.py
@@ -2,5 +2,21 @@
 
 '''
 Soru: Girilen bir sayının asal olup olmadığını bulun.
-
 '''
+
+sayi = int(input('Sayı: '))
+
+asalmi = True
+
+if sayi == 1:
+    asalmi = False
+
+for i in range(2,sayi):
+    if sayi % i == 0:
+        asalmi = False
+        break
+
+if asalmi:
+    print('Sayı asaldır.')
+else:
+    print('Sayı asal değildir.')
